f006e22adf8ede10680692a6308ba25ad3758561
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-07-22 21:05:07 +0100

    completed tests - 3 tests

--- tests/compare_test.py
+++ tests/compare_test.py
@@ -3,6 +3,12 @@ import unittest
 from src.compare import compare
 
 class TestCompare(unittest.TestCase):
-
+    # comparing two numbers test either one greater, one less than, or equal
     def test_compare_3_1_returns_3_is_greater_than_1(self):
         self.assertEqual("3 is greater than 1", compare(3, 1))
+
+    def test_compare_3_1_returns_3_is_less_than_1(self):
+        self.assertEqual("1 is less than 3", compare(1, 3))
+    
+    def test_compare_3_1_returns_3_is_equal_to_3(self):
+        self.assertEqual("3 is equal to 3", compare(3, 3))
